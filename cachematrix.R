f8306707e1b3e4d3db8ecab557b1b8e75485cb44
Juliano Dawid Barboza <julianodawid@gmail.com>
2015-02-22 17:00:12 -0300

    Added description for cachematrix
    Added general description of the cachematrix.R file, specific
    descriptions for the two implemented functions (makeCacheMatrix
    and cacheSolve) and example use.

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Theese functions provide a way to get the inverse of a matrix from a 
+## cached value when it has already been calculated before.
 
-## Write a short comment describing this function
+## The makeCacheMatrix(x) function creates a list with getters and setters
+## function to access the provided matrix (x) and its inverse. When
+## setting a new matrix, its inverse is set to NULL.
 
 makeCacheMatrix <- function(x = matrix()) {
+        ## Creates an object that can provide the cached value of 
+        ## a matrix's inverse
         i <- NULL
         set <- function(y) {
                 x <<- y
@@ -18,7 +22,14 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve(x) returns the inverse of the matrix stored in x (where
+## x is an object created by makeCacheMatrix). This function verifies if
+## the value stored as the inverse (x$getinverse()) is not NULL, in which 
+## case it just returns the cached value x$getinverse(). If, on the other
+## hand, x$getinverse() is NULL, than the inverse is calculated with
+## solve(), stored with x$setinverse() and returned to the user. When the 
+## returned value comes from the cache, the message 'getting cached data'
+## is displayed.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -32,3 +43,11 @@ cacheSolve <- function(x, ...) {
         x$setinverse(i)
         i
 }
+
+## Example use:
+## x <- makeCacheMatrix(matrix(1:4,2,2))
+## cacheSolve(x) ## First time the inverse is calculated
+## cacheSolve(x) ## Cached value ir provided
+## x$set(matrix(rnorm(9),3,3)) ## set new matrix
+## cacheSolve(x) ## First time the inverse is calculated
+## cacheSolve(x) ## Cached value ir provided
